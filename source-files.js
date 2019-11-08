692cd44484bae6d0d78ed8a69607fa13f6f9b089
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-08 19:10:47 +0000

    Deploy Michael-F-Bryan/xsd-rs to github.com/Michael-F-Bryan/xsd-rs.git:gh-pages

--- source-files.js
+++ source-files.js
@@ -6,5 +6,5 @@ sourcesIndex["thiserror"] = {"name":"","files":["aserror.rs","lib.rs"]};
 sourcesIndex["thiserror_impl"] = {"name":"","files":["ast.rs","attr.rs","expand.rs","fmt.rs","lib.rs","prop.rs","valid.rs"]};
 sourcesIndex["unicode_xid"] = {"name":"","files":["lib.rs","tables.rs"]};
 sourcesIndex["xml"] = {"name":"","dirs":[{"name":"reader","dirs":[{"name":"parser","files":["inside_cdata.rs","inside_closing_tag_name.rs","inside_comment.rs","inside_declaration.rs","inside_doctype.rs","inside_opening_tag.rs","inside_processing_instruction.rs","inside_reference.rs","mod.rs","outside_tag.rs"]}],"files":["config.rs","error.rs","events.rs","lexer.rs","mod.rs"]},{"name":"writer","files":["config.rs","emitter.rs","events.rs","mod.rs"]}],"files":["attribute.rs","common.rs","escape.rs","lib.rs","macros.rs","name.rs","namespace.rs","util.rs"]};
-sourcesIndex["xsd"] = {"name":"","files":["errors.rs","lib.rs","parse.rs","types.rs"]};
+sourcesIndex["xsd"] = {"name":"","files":["lib.rs"]};
 createSourceSidebar();
